a2cf783d9b1eeaca95c8207fb1275db7d332a106
MohamedRamadan7 <mohamedramadan6420@Gmail.com>
2025-07-23 14:31:02 +0300

    add FLAG SECURE

--- example/android/app/src/main/kotlin/org/jitsi/jitsi_meet_flutter_sdk_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/org/jitsi/jitsi_meet_flutter_sdk_example/MainActivity.kt
@@ -1,6 +1,14 @@
 package org.jitsi.jitsi_meet_flutter_sdk_example
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import android.view.WindowManager.LayoutParams
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        window.addFlags(LayoutParams.FLAG_SECURE)
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
